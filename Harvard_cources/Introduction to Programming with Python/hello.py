dc5b9b0b26c4b06579868048cafd1914384f9176
amirabbas afrasiyabi <119659555+AmirabbasAfrasiyabi@users.noreply.github.com>
2024-09-07 15:17:39 +0330

    Add files via upload

---
+++ Harvard_cources/Introduction to Programming with Python/hello.py
@@ -0,0 +1,16 @@
+
+#function
+
+def hello():
+    name = input("what is your name?")
+    print(name)
+hello()
+
+def main():
+    name = input("what is your names::")
+    heloo(name)
+def heloo(to = "world"):
+    print("heloo",to)
+
+main()
+
