3a6460e93c9ddf5c19efe31c6410ac0a6359760a
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-09-07 18:25:13 +0330

    created list

---
+++ Harvard_cources/Introduction to Programming with Python/hagwards.py
@@ -0,0 +1,19 @@
+#list
+student = ["amir","atiye",'reyhane']
+print(student[0])
+print(student[1])
+print(student[2])
+
+student.append('harry')  
+student.reverse()
+student.insert(2,'ali')
+student.remove('amir')
+print(student)
+
+
+for i in range (len(student)):
+    print(student[i])
+
+#dictionary
+
+
